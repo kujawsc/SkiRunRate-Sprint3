5a0cbd06afa7f82c7e1312b6dcea057f949df152
kujawsc <kujawsc@gmail.com>
2017-11-19 19:24:14 -0500

    11/19/17 -- Sprint 3 changes

---
+++ SkiRunRater.Sprint3.Solution/DAL/SkiRunRepositoryXML.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace SkiRunRater
+{
+    /// <summary>
+    /// method to write all ski run information to the date file
+    /// </summary>
+    public class SkiRunRepositoryXML : IDisposable
+    {
+        private List<SkiRun> _skiRuns;
+
+        public SkiRunRepositoryXML()
+        {
+            _skiRuns = ReadSkiRunsData(DataSettings.dataFilePath);
+        }
+
+        /// <summary>
+        /// method to read all ski run information from the data file and return it as a list of SkiRun objects
+        /// </summary>
+        /// <param name="dataFilePath">path the data file</param>
+        /// <returns>list of SkiRun objects</returns>
+        public List<SkiRun> ReadSkiRunsData(string dataFilePath)
+        {
+            List<SkiRun> skiRuns;
+
+            XmlSerializer serializer = new XmlSerializer(typeof(List<SkiRun>), new XmlRootAttribute("SkiRuns"));
+
+            using (FileStream stream = File.OpenRead(DataSettings.dataFilePath))
+            {
+                skiRuns = (List<SkiRun>)serializer.Deserialize(stream);
+            }
+
+            return skiRuns;
+        }
+
+        /// <summary>
+        /// method to write all of the list of ski runs to the XML file
+        /// </summary>
+        public void WriteSkiRunsData()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<SkiRun>), new XmlRootAttribute("SkiRuns"));
+
+            using (FileStream stream = File.OpenWrite(DataSettings.dataFilePath))
+            {
+                serializer.Serialize(stream, _skiRuns);
+            }
+        }
+
+        /// <summary>
+        /// method to add a new ski run
+        /// </summary>
+        /// <param name="skiRun"></param>
+        public void InsertSkiRun(SkiRun skiRun)
+        {
+            _skiRuns.Add(skiRun);
+
+            WriteSkiRunsData();
+        }
+
+        /// <summary>
+        /// method to delete a ski run by ski run ID
+        /// </summary>
+        /// <param name="ID"></param>
+        public void DeleteSkiRun(int ID)
+        {
+            _skiRuns.RemoveAll(sr => sr.ID == ID);
+
+            WriteSkiRunsData();
+        }
+
+        /// <summary>
+        /// method to update an existing ski run
+        /// </summary>
+        /// <param name="skiRun">ski run object</param>
+        public void UpdateSkiRun(SkiRun skiRun)
+        {
+            DeleteSkiRun(skiRun.ID);
+            InsertSkiRun(skiRun);
+
+            WriteSkiRunsData();
+        }
+
+        /// <summary>
+        /// method to return a ski run object given the ID
+        /// </summary>
+        /// <param name="ID">int ID</param>
+        /// <returns>ski run object</returns>
+        public SkiRun GetSkiRunByID(int ID)
+        {
+            SkiRun skiRun = null;
+
+            skiRun = _skiRuns.FirstOrDefault(sr => sr.ID == ID);
+
+            return skiRun;
+        }
+
+        /// <summary>
+        /// method to return a list of ski run objects
+        /// </summary>
+        /// <returns>list of ski run objects</returns>
+        public List<SkiRun> GetSkiAllRuns()
+        {
+            return _skiRuns;
+        }
+
+        /// <summary>
+        /// method to query the data by the vertical of each ski run in feet
+        /// </summary>
+        /// <param name="minimumVertical">int minimum vertical</param>
+        /// <param name="maximumVertical">int maximum vertical</param>
+        /// <returns></returns>
+        public List<SkiRun> QueryByVertical(int minimumVertical, int maximumVertical)
+        {
+            List<SkiRun> matchingSkiRuns = new List<SkiRun>();
+
+            //
+            // use a lambda expression with the Where method to query
+            //
+            matchingSkiRuns = _skiRuns.Where(sr => sr.Vertical >= minimumVertical && sr.Vertical <= maximumVertical).ToList();
+
+            return matchingSkiRuns;
+        }
+
+        /// <summary>
+        /// method to handle the IDisposable interface contract
+        /// </summary>
+        public void Dispose()
+        {
+            _skiRuns = null;
+        }
+    }
+}
---
+++ SkiRunRater.Sprint3.Starter/ReadMe.txt
@@ -0,0 +1,7 @@
+﻿Title: Ski Run Rater Sprint 3
+Description: Show the functionality unique method created for each case statement block
+and called from the case statement block. As well applied functions as intended.
+Application Type: Console
+Author: Catherine Kujawski
+Dated: 11/21/17
+Last Modified: 11/24/17
